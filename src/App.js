547b52f77d84466cbcdeeb8b00a28eb5300266c3
waellouat <116644467+waellouat@users.noreply.github.com>
2023-05-11 11:39:41 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,46 +1,41 @@
-import React, { Component } from "react" ;
+import React, { Component } from "react";
 import Persone from "./component/Profil";
-import "./App.css" ;
- export default class App extends React.Component {
-  constructor () {
-  super()
-  this.state={
-    show : true ,
-    count : 0 ,
-intervall : null 
-  }
-  
-  }
-  componentDidMount(){
-    console.log(" componentDidMount")
-    this.setState ({
-     intervall : setInterval (
-         ()=>this.setState({count : this.state.count +1 }) ,
-         1000
-     )
-     
-    })
-  }
-  componentDidUpdate () {
-     console.log ("il y un update")
-  }
-  Show = () => {
-    this.setState({show :!this.state.show})
-    console.log(this.state.show)
-  }
-  
-render() {
-return(
-  <div>
-    {this.state.show && <Persone/> }
-<button onClick={this.Show} >click me</button>
-<hr/>
-<h2>count</h2>
-<h3>{this.state.count} </h3>
+import "./App.css";
 
-  </div>
-)
+export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      show: true,
+      count: 0,
+      interval: null,
+    };
+  }
 
+  componentDidMount() {
+    console.log("componentDidMount");
+    this.setState({
+      interval: setInterval(() => this.setState({ count: this.state.count + 1 }), 1000),
+    });
+  }
+
+  componentDidUpdate() {
+    console.log("There is an update");
+  }
+
+  toggleShow = () => {
+    this.setState((prevState) => ({ show: !prevState.show }));
+  };
+
+  render() {
+    return (
+      <div>
+        {this.state.show && <Persone />}
+        <button onClick={this.toggleShow}>Toggle Profile</button>
+        <hr />
+        <h2>Count:</h2>
+        <h3>{this.state.count}</h3>
+      </div>
+    );
+  }
 }
-};
-
